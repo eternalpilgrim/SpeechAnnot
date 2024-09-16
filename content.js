b5480d71c53a8e7aa4dac2fa0fc668c7871e63e6
eternalpilgrim8 <64610994+eternalpilgrim@users.noreply.github.com>
2024-09-16 10:02:48 +0300

    Add files via upload

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 chrome.runtime.onMessage.addListener((record_text) => {
   //console.log("Message from the background script:");
   //console.log(record_text);
-	let input_in_focus = document.querySelector('input:focus');
-	input_in_focus.value += record_text+" ";
+	let input_in_focus;
+	input_in_focus = document.querySelector('input:focus');
+	//if (input_in_focus==null) input_in_focus = document.querySelector("textarea");
+	//input_in_focus.value += record_text+" ";
+	navigator.clipboard.writeText(record_text);
 });
 
